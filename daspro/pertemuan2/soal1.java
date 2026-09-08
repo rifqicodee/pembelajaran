27558216f3e336c3c35bf1a7268a041c3e12e24d
M Rifqi Aziz <mrifqiaziz67@gmail.com>
2026-09-09 00:23:49 +0700

    1.2

--- daspro/pertemuan2/soal1.java
+++ daspro/pertemuan2/soal1.java
@@ -7,8 +7,7 @@ public class soal1 {
         int a = 3;
         int b = 2;
         int c = 1;
-        int kuadrat = a * a;
-        int hasil = 2 * kuadrat + a * b + c;
+        int hasil = 2 * a * a + a * b + c;
         System.out.println("------------------------------------------------");
         System.out.println("Menghitung persamaan kuadrat x = 2a^2 + ab + c");
         System.out.println("------------------------------------------------");
